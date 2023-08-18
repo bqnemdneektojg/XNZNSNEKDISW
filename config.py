b94dcd898ec3d228eed91c85a782dffbdc93f8e8
bqnemdneektojg <141013246+bqnemdneektojg@users.noreply.github.com>
2023-08-18 22:55:57 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,33 @@
+
+#////////////////////// НАСТРОЙКИ БОТА \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+BOT_TOKEN = ''# токен бота
+
+USERNAME = '@aurora_us' #юзернейм бота с @
+
+ADMIN =  6152765999 #ID админа [если несколько - через запятую]
+
+ADMIN_LINK = 'https://t.me/aurora_us' #Ссылка на админа
+
+CHANNEL_LINK = 'https://t.me/aurora_us' #Ссылка на новостной канал
+
+
+
+#///////////////////// НАСТРОЙКИ API \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+PROXY_KEY = '' #Ключ proxyline.net
+
+QIWI_KEY = '0' #QIWI P2P ключ
+
+
+
+
+#//////////////////////// Картинки \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+MENU_IMAGE = 'https://i.imgur.com/PJoKuUT.jpg' #Картинка в меню
+
+PROXY_IMAGE = 'https://i.imgur.com/PJoKuUT.jpg' #Картинка в прокси воронке
+
+PROFILE_IMAGE = 'https://i.imgur.com/PJoKuUT.jpg' #Картинка в профиле
+
+
